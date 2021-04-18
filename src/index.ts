c9afe0021ada93ddda84f67b35f9f0635974c053
Matheus Batistela <matheushenriquebatistela@gmail.com>
2021-04-18 00:09:44 -0300

    starting scenes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import Phaser from 'phaser';
 import config from './config';
-// import GameScene from './scenes/Game';
-import KitchenScene from './scenes/kitchen'
+import GameScene from './scenes/Game';
+import KitchenScene from './scenes/Kitchen'
+import UIScene from './scenes/UIScene';
 
 new Phaser.Game(
   Object.assign(config, {
-    scene: [KitchenScene]
+    scene: [GameScene,KitchenScene,UIScene]
   })
 );
---
+++ src/scenes/PetScene.ts
@@ -0,0 +1,15 @@
+import Phaser from 'phaser';
+
+export default class PetScene extends Phaser.Scene {
+  constructor() {
+    super('pet');
+  }
+
+  preload() {
+
+  }
+
+  create() {
+      
+  }
+}
